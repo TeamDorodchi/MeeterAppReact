1bd1e7b25acc06282ccd8bac9004595079bdf8db
kempshane22 <skemp4@uncc.edu>
2021-04-29 19:03:42 -0400

    Revert "Removed unnecessary files"
    This reverts commit 99690b89

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,24 +1,18 @@
 import React, {SyntheticEvent, Component}  from 'react';
-
 import ApiCalendar from 'react-google-calendar-api';
 import App from '../src/App.js'
-import Calendar from "@ericz1803/react-google-calendar";
 
 
 class HomePage extends React.Component{
-    render(): ReactNode{
-        return (
-            <div>
-                <p>Add application description here</p>
-                <button onClick="loginFun()">Login</button>
-            </div>
 
-        );
-    }
 
 
 
 
 }
+
+
+
+
 
 export default HomePage;
