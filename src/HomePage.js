99690b89c55ab7bda152a80f73feaa6a1bc3ff9f
kempshane22 <skemp4@uncc.edu>
2021-04-29 18:10:48 -0400

    Removed unnecessary files

---
+++ src/HomePage.js
@@ -0,0 +1,18 @@
+import React, {SyntheticEvent, Component}  from 'react';
+import ApiCalendar from 'react-google-calendar-api';
+import App from '../src/App.js'
+
+
+class HomePage extends React.Component{
+
+
+
+
+
+}
+
+
+
+
+
+export default HomePage;
